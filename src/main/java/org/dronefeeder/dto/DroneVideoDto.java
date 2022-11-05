aac4ef3bc0b0e4b0150d09e62b7bbf544fe987e8
Brenojbs <brenojbs@hotmail.com>
2022-11-05 12:35:09 -0300

    Feat: Criado endpoint DroneVideo

---
+++ src/main/java/org/dronefeeder/dto/DroneVideoDto.java
@@ -0,0 +1,15 @@
+package org.dronefeeder.dto;
+
+public class DroneVideoDto {
+
+  private String url;
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+}
---
+++ src/main/java/org/dronefeeder/service/DroneVideoService.java
@@ -0,0 +1,53 @@
+package org.dronefeeder.service;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+import org.dronefeeder.commons.DroneVideoNaoEncontradoException;
+import org.dronefeeder.commons.ErroInesperadoException;
+import org.dronefeeder.dto.DroneVideoDto;
+import org.dronefeeder.entity.DroneVideoEntity;
+
+@ApplicationScoped
+public class DroneVideoService {
+
+  @Transactional
+  public void salvar(DroneVideoDto dto) {
+    DroneVideoEntity droneVideo = new DroneVideoEntity();
+    droneVideo.setUrl(dto.getUrl());
+    droneVideo.persist();
+  }
+
+  public DroneVideoEntity buscarId(Long id) {
+    try {
+      DroneVideoEntity drone = DroneVideoEntity.findById(id);
+
+      if (drone == null) {
+        throw new DroneVideoNaoEncontradoException();
+      }
+
+      return drone;
+
+    } catch (DroneVideoNaoEncontradoException e) {
+      throw new DroneVideoNaoEncontradoException();
+    } catch (Exception e) {
+      throw new ErroInesperadoException();
+    }
+  }
+
+  @Transactional
+  public void deletar(Long id) throws DroneVideoNaoEncontradoException {
+
+    try {
+      boolean deleted = DroneVideoEntity.deleteById(id);
+
+      if (!deleted) {
+        throw new DroneVideoNaoEncontradoException();
+      }
+
+    } catch (DroneVideoNaoEncontradoException e) {
+      throw new DroneVideoNaoEncontradoException();
+    } catch (Exception e) {
+      throw new ErroInesperadoException();
+    }
+  }
+}
